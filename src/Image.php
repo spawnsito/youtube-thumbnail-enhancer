2497f005ecb6e462ef2876d52522a13ed7d31b81
Daniel Cruz <spawnsito@gmail.com>
2016-04-02 14:41:22 +0200

    Test add hq play icon

--- src/Image.php
+++ src/Image.php
@@ -5,11 +5,22 @@ class Image
     const HIGH_QUALITY = 'hq';
     const MEDIUM_QUALITY = 'mq';
 
+    const HIGH_QUALITY_WIDTH = 480;
+    const HIGH_QUALITY_HEIGHT = 270;
+    const HIGH_QUALITY_COPY_WIDTH = 480;
+    const HIGH_QUALITY_COPY_HEIGHT = 360;
+    const HIGH_QUALITY_OFFSET_TOP = 45;
+    const HIGH_QUALITY_OFFSET_LEFT = 0;
+
+    const ICON_FILENAME = 'Resources/play-{quality}.png';
+
     private $canvas;
+    private $quality;
 
     public function __construct($path, $quality = self::MEDIUM_QUALITY)
     {
         $this->canvas = imagecreatefromjpeg($path);
+        $this->quality = $quality;
         if ($quality == self::HIGH_QUALITY) {
             $this->convertToHighQuality();
         }
@@ -23,11 +34,39 @@ class Image
 
     private function convertToHighQuality()
     {
-        $cleft = 0;
-        $ctop = 45;
-        $canvas = imagecreatetruecolor(480, 270);
-        imagecopy($canvas, $this->canvas, 0, 0, $cleft, $ctop, 480, 360);
+        $canvas = imagecreatetruecolor(self::HIGH_QUALITY_WIDTH, self::HIGH_QUALITY_HEIGHT);
+        imagecopy($canvas, $this->canvas, 0, 0, self::HIGH_QUALITY_OFFSET_LEFT, self::HIGH_QUALITY_OFFSET_TOP, self::HIGH_QUALITY_COPY_WIDTH, self::HIGH_QUALITY_COPY_HEIGHT);
         $this->canvas = $canvas;
+    }
+
+    public function addPlayIcon()
+    {
+        $imageWidth = imagesx($this->canvas);
+        $imageHeight = imagesy($this->canvas);
+
+        $play_icon = str_replace('{quality}', $this->quality, self::ICON_FILENAME);
+
+        $logoImage = imagecreatefrompng(__DIR__ . '/' . $play_icon);
+        imagealphablending($logoImage, true);
+
+        $logoWidth = imagesx($logoImage);
+        $logoHeight = imagesy($logoImage);
+
+        $left = round($imageWidth / 2) - round($logoWidth / 2);
+        $top = round($imageHeight / 2) - round($logoHeight / 2);
+
+        $copyFilename = 'copy_png.png';
+        imagecopy($this->canvas, $logoImage, $left, $top, 0, 0, $logoWidth, $logoHeight);
+        imagepng($this->canvas, $copyFilename, 9);
+
+        $input = imagecreatefrompng($copyFilename);
+        $output = imagecreatetruecolor($imageWidth, $imageHeight);
+        $white = imagecolorallocate($output, 255, 255, 255);
+        imagefilledrectangle($output, 0, 0, $imageWidth, $imageHeight, $white);
+        imagecopy($output, $input, 0, 0, 0, 0, $imageWidth, $imageHeight);
+        $this->canvas = $output;
+
+        @unlink($copyFilename);
     }
 
 }
--- src/Tests/ImageTest.php
+++ src/Tests/ImageTest.php
@@ -25,4 +25,30 @@ class ImageTest extends PHPUnit_Framework_TestCase
         $this->assertEquals(480, imagesx($image->obtainImage()));
         $this->assertEquals(270, imagesy($image->obtainImage()));
     }
+
+    public function testCreateImageWithMediumQuality()
+    {
+        $path = __DIR__ . '/fixtures/image.jpg';
+        $image = new Image($path);
+
+        $this->assertEquals(320, imagesx($image->obtainImage()));
+        $this->assertEquals(180, imagesy($image->obtainImage()));
+    }
+
+    public function testAddPlayIcon()
+    {
+        $path = __DIR__ . '/fixtures/image-hq-no-processed.jpg';
+        $image = new Image($path, 'hq');
+        $image->addPlayIcon();
+        
+        $outputPath = __DIR__ . '/fixtures/image_create_play_icon.jpg';
+        imagejpeg($image->obtainImage(), $outputPath, 95);
+
+        $expectedPath = __DIR__ . '/fixtures/image-hq-play.jpg';
+        $this->assertFileEquals($expectedPath, $outputPath);
+
+        unlink($outputPath);
+    }
+
+
 }
